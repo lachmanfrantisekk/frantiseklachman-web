b652fb84412970610c4c8cc2a3a285d8709861ae
lachmanfrantisekk <lachmanfrantisekk@gmail.com>
2026-07-31 22:39:08 +0200

    Create and update questions.js

---
+++ coffeepicker/questions.js
@@ -0,0 +1,159 @@
+const questions = [
+
+    {
+        id: "caffeine",
+
+        title: "Chceš kofein?",
+
+        answers: [
+
+            {
+                text: "Ano",
+                value: true
+            },
+
+            {
+                text: "Bez kofeinu",
+                value: false
+            }
+
+        ]
+
+    },
+
+    {
+
+        id: "temperature",
+
+        title: "Jak ji chceš?",
+
+        answers: [
+
+            {
+                text: "☕ Horkou",
+                value: "hot"
+            },
+
+            {
+                text: "🧊 Ledovou",
+                value: "iced"
+            }
+
+        ]
+
+    },
+
+    {
+
+        id: "cup",
+
+        title: "Jak velký šálek?",
+
+        answers: [
+
+            {
+                text: "Espresso (40 ml)",
+                value: "espresso"
+            },
+
+            {
+                text: "Double Espresso (80 ml)",
+                value: "double"
+            },
+
+            {
+                text: "Mug (230 ml)",
+                value: "mug"
+            }
+
+        ]
+
+    },
+
+    {
+
+        id: "milk",
+
+        title: "Přidáš mléko?",
+
+        answers: [
+
+            {
+                text: "Ano",
+                value: true
+            },
+
+            {
+                text: "Ne",
+                value: false
+            }
+
+        ]
+
+    },
+
+    {
+
+        id: "intensity",
+
+        title: "Jak silnou?",
+
+        answers: [
+
+            {
+                text: "Jemnou",
+                value: "light"
+            },
+
+            {
+                text: "Střední",
+                value: "medium"
+            },
+
+            {
+                text: "Silnou",
+                value: "strong"
+            }
+
+        ]
+
+    },
+
+    {
+
+        id: "taste",
+
+        title: "Na co máš chuť?",
+
+        answers: [
+
+            {
+                text: "🍫 Čokoládová",
+                value: "chocolate"
+            },
+
+            {
+                text: "🌰 Oříšková",
+                value: "nutty"
+            },
+
+            {
+                text: "🍓 Ovocná",
+                value: "fruity"
+            },
+
+            {
+                text: "🌸 Květinová",
+                value: "floral"
+            },
+
+            {
+                text: "🍮 Sladká / Vanilková",
+                value: "sweet"
+            }
+
+        ]
+
+    }
+
+];
