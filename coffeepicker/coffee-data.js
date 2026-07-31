6ddec5286dbfc61b198b50d1407453910daf428b
lachmanfrantisekk <lachmanfrantisekk@gmail.com>
2026-07-31 22:51:03 +0200

    Create and update coffee-data.js

---
+++ coffeepicker/coffee-data.js
@@ -0,0 +1,317 @@
+const coffees = [
+
+{
+    id: "white-chocolate-strawberry",
+
+    name: "White Chocolate & Strawberry",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: true,
+
+    intensity: "light",
+
+    tastes: [
+        "sweet",
+        "fruity"
+    ]
+},
+
+{
+    id: "coconut-vanilla-over-ice",
+
+    name: "Coconut Vanilla Over Ice",
+
+    caffeine: true,
+
+    iced: true,
+
+    cup: "mug",
+
+    milk: false,
+
+    intensity: "light",
+
+    tastes: [
+        "sweet",
+        "vanilla"
+    ]
+},
+
+{
+    id: "pumpkin-spice-cake",
+
+    name: "Pumpkin Spice Cake",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: true,
+
+    intensity: "medium",
+
+    tastes: [
+        "sweet",
+        "spicy"
+    ]
+},
+
+{
+    id: "lavender-vanilla-decaf",
+
+    name: "French Lavender & Vanilla Decaffeinato",
+
+    caffeine: false,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: true,
+
+    intensity: "light",
+
+    tastes: [
+        "floral",
+        "vanilla"
+    ]
+},
+
+{
+    id: "double-espresso-dolce",
+
+    name: "Double Espresso Dolce",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "double",
+
+    milk: true,
+
+    intensity: "medium",
+
+    tastes: [
+        "sweet",
+        "cereal"
+    ]
+},
+
+{
+    id: "costa-rica",
+
+    name: "Costa Rica",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: false,
+
+    intensity: "medium",
+
+    tastes: [
+        "cereal",
+        "fruity"
+    ]
+},
+
+{
+    id: "ethiopia",
+
+    name: "Ethiopia",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: false,
+
+    intensity: "light",
+
+    tastes: [
+        "floral",
+        "fruity"
+    ]
+},
+
+{
+    id: "melozio-decaf",
+
+    name: "Melozio Decaffeinato",
+
+    caffeine: false,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: true,
+
+    intensity: "light",
+
+    tastes: [
+        "sweet",
+        "cereal"
+    ]
+},
+
+{
+    id: "colombia",
+
+    name: "Colombia",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: false,
+
+    intensity: "medium",
+
+    tastes: [
+        "fruity"
+    ]
+},
+
+{
+    id: "bianco-doppio",
+
+    name: "Bianco Doppio",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "double",
+
+    milk: true,
+
+    intensity: "medium",
+
+    tastes: [
+        "sweet",
+        "cereal"
+    ]
+},
+
+{
+    id: "sweet-vanilla-decaf",
+
+    name: "Sweet Vanilla Decaffeinato",
+
+    caffeine: false,
+
+    iced: false,
+
+    cup: "mug",
+
+    milk: true,
+
+    intensity: "light",
+
+    tastes: [
+        "vanilla",
+        "sweet"
+    ]
+},
+
+{
+    id: "pistachio-vanilla-over-ice",
+
+    name: "Pistachio Vanilla Flavour Over Ice",
+
+    caffeine: true,
+
+    iced: true,
+
+    cup: "mug",
+
+    milk: true,
+
+    intensity: "light",
+
+    tastes: [
+        "vanilla",
+        "sweet",
+        "nutty"
+    ]
+},
+
+{
+    id: "altissio",
+
+    name: "Altissio",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "espresso",
+
+    milk: true,
+
+    intensity: "strong",
+
+    tastes: [
+        "chocolate"
+    ]
+},
+
+{
+    id: "double-espresso-scuro",
+
+    name: "Double Espresso Scuro",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "double",
+
+    milk: true,
+
+    intensity: "strong",
+
+    tastes: [
+        "chocolate",
+        "roasted"
+    ]
+},
+
+{
+    id: "double-espresso-chiaro",
+
+    name: "Double Espresso Chiaro",
+
+    caffeine: true,
+
+    iced: false,
+
+    cup: "double",
+
+    milk: true,
+
+    intensity: "medium",
+
+    tastes: [
+        "wood",
+        "chocolate"
+    ]
+}
+
+];
